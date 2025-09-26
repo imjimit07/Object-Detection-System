4eff1084a39b47b7469aab0f20fe470b2d080833
Jimit Patel <jimit0907@gmail.com>
2025-09-26 23:53:16 +0530

    added video source

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import torch
 from ultralytics import YOLO
 import time
+import cvzone
+import math
+import cv2
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 if device == "cuda":
@@ -11,17 +14,58 @@ else:
 
 model = YOLO("../Yolo-Weights/yolov8l.pt").to(device)
 
-classnames = ["person", "pen", "bottle", "phone", "headphones"]
+classnames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush", "comb"]
 
 prev_frame_time = 0
 new_frame_time = 0
 
+# capture sources
+'''
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+'''
+cap = cv2.VideoCapture("test.mp4")  # video
+
 while True:
     new_frame_time = time.time()
-    results = model.predict(source=0, conf=0.5, device=device, show=True) # detect objects in webcam feed
+    success, img = cap.read()
+    if not success:
+        print("Camera capture failed")
+        break
+
+    results = model(img, stream=True)
     for r in results:
-        print(r)
-        for box in r.boxes:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
             cls = int(box.cls[0])
-            if classnames[cls] in [""person", "pen", "bottle", "phone", "headphones""]:
-                print(f"Detected: {classnames[cls]} with confidence {box.conf[0]:.2f}")
+
+            cvzone.putTextRect(img, f'{classnames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=2, thickness=2)
+
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+    print(f"FPS: {fps:.2f}")
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
