d8ee8bbcedee80d33152da37d13450e4afb7d1c6
Jimit Patel <jimit0907@gmail.com>
2025-09-26 22:51:27 +0530

    define YOLO

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+from ultralytics import YOLO
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 if device == "cuda":
@@ -6,3 +7,10 @@ if device == "cuda":
     print(f"Using GPU: {gpuname}")
 else:
     print("Using CPU")
+
+model = YOLO("../Yolo-Weights/yolov8l.pt").to(device)
+
+classnames = ["person", "pen", "bottle", "phone", "headphones"]
+
+prev_frame_time = 0
+new_frame_time = 0
